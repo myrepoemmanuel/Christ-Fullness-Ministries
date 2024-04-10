27840d04e98d7279be84d9a25afb473afb48e20e
Ak-automas <125151159+Ak-automas@users.noreply.github.com>
2024-04-11 00:20:49 +0300

    Update settings.py

--- christ_fullness/settings.py
+++ christ_fullness/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://christ-fullness-ministries.onrender.com", "https://christfm.org","https://www.christfm.org", "www.christfm.org"]
 
