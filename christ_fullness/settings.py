5c518c082247fc91dbed829551fa163a6e70647e
Ak-automas <125151159+Ak-automas@users.noreply.github.com>
2024-07-20 00:11:02 +0300

    update allowed hosts

--- christ_fullness/settings.py
+++ christ_fullness/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["christ-fullness-ministries.onrender.com","christfm.org", '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["christ-fullness-ministries.onrender.com", "christ-fullness-ministries-1.onrender.com","christfm.org", '127.0.0.1', 'localhost']
 
 
 # Application definition
